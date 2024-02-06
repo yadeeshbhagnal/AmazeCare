c6e8106167d89a7f815aeaa94fc68ffea9625711
--replace-all <==>
2024-02-06 16:58:12 +0530

    resolving conflicts

--- AmazeCare/src/main/java/com/hexaware/amazecare/repository/AppointmentRepository.java
+++ AmazeCare/src/main/java/com/hexaware/amazecare/repository/AppointmentRepository.java
@@ -1,9 +1,6 @@
 package com.hexaware.amazecare.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hexaware.amazecare.entities.Appointment;
--- AmazeCare/src/main/java/com/hexaware/amazecare/repository/PatientRepository.java
+++ AmazeCare/src/main/java/com/hexaware/amazecare/repository/PatientRepository.java
@@ -2,7 +2,6 @@ package com.hexaware.amazecare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.hexaware.amazecare.entities.Patient;
 
 @Repository
