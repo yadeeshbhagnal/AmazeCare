ea734bcb5f9d3e27d328b35ee80418bbdace3bf9
--replace-all <==>
2024-02-08 23:06:45 +0530

    resolving

---
+++ AmazeCare/src/main/java/com/hexaware/amazecare/service/IPatientService.java
@@ -0,0 +1,27 @@
+package com.hexaware.amazecare.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.hexaware.amazecare.dto.AppointmentDto;
+import com.hexaware.amazecare.dto.DoctorDto;
+import com.hexaware.amazecare.dto.PatientDto;
+import com.hexaware.amazecare.entities.Appointment;
+import com.hexaware.amazecare.entities.Doctor;
+import com.hexaware.amazecare.entities.Patient;
+
+public interface IPatientService {
+	
+	public boolean updatePatientInfo(PatientDto patientDto);
+	
+	public boolean scheduleAppointment(AppointmentDto appointmentDto);
+	
+	public String rescheduleAppointment(int appointmentId, LocalDate date);
+	
+	public String cancelAppointment(int appointmentId);
+	
+	public List<Appointment> viewAppointments(int patientId);
+		
+	public List<Doctor> getDocBySpeciality(String speciality);
+	
+}
