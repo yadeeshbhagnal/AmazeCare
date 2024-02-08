0261bc5679f54d04edac443e0b6eac32cc12ee6a
yadeeshbhagnal <yadeesh826@gmail.com>
2024-02-08 22:32:02 +0530

    deleted

--- AmazeCare/src/main/java/com/hexaware/amazecare/repository/RecommendedTestsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.amazecare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hexaware.amazecare.entities.RecommendedTests;
-
-public interface RecommendedTestsRepository extends JpaRepository<RecommendedTests, Integer>{
-
-}
