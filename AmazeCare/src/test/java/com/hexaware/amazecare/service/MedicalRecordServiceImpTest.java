8c0c54c526e8f728570661b673f69874af5e894c
yadeeshbhagnal <yadeesh826@gmail.com>
2024-02-23 11:06:43 +0530

    Penidng test cases pushed

--- AmazeCare/src/test/java/com/hexaware/amazecare/service/MedicalRecordServiceImpTest.java
+++ AmazeCare/src/test/java/com/hexaware/amazecare/service/MedicalRecordServiceImpTest.java
@@ -1,31 +1,60 @@
 package com.hexaware.amazecare.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.hexaware.amazecare.entities.MedicalRecord;
+import com.hexaware.amazecare.repository.MedicalRecordRepository;
+
+@SpringBootTest
 class MedicalRecordServiceImpTest {
 	
+	@Autowired
+	IMedicalRecordService medicalService;
 	
+	@Autowired
+	MedicalRecordRepository medicalRecordRepository;
+	
+	@BeforeEach
+	void setUp() {
+		Authentication auth = new UsernamePasswordAuthenticationToken("Chris11","chrish123"); 
+		SecurityContextHolder.getContext().setAuthentication(auth);
+	}
 
 	@Test
 	void testViewMedicalRecordDoc() {
-		fail("Not yet implemented");
+		List<MedicalRecord> list = medicalService.viewMedicalRecordDoc(1);
+		assertNotNull(list);
+		
 	}
 
 	@Test
 	void testViewMedicalRecordPatient() {
-		fail("Not yet implemented");
+		List<MedicalRecord> list = medicalService.viewMedicalRecordPatient();
+		assertNotNull(list);
 	}
 
 	@Test
 	void testViewRecommendedTests() {
-		fail("Not yet implemented");
+		List<MedicalRecord> list = medicalService.viewMedicalRecordDoc(1);
+		assertNotNull(list);
 	}
 
 	@Test
 	void testViewRecommendedMedicine() {
-		fail("Not yet implemented");
+		List<MedicalRecord> list = medicalService.viewMedicalRecordDoc(1);
+		assertNotNull(list);
 	}
 
 }
